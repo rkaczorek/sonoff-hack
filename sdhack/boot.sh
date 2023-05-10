311001cd091982e667dc4e1bfc0bb735a450a9df
Radek Kaczorek <rkaczorek@gmail.com>
2023-05-10 22:01:19 +0200

    Fix wifi connection for GK-200MP2C V0525.1.276build20201103

--- sdhack/boot.sh
+++ sdhack/boot.sh
@@ -10,6 +10,7 @@ mount --bind /var/sdcard/log /var/log
 touch /tmp/di.wav
 touch /tmp/Internet_connected_Welcome_to_use_cloud_camera.wav
 touch /tmp/WiFi_connect_success.wav
+touch /tmp/Please_use_mobile_phone_for_WiFi_configuration.wav
 
 WAV_FILE_DIR=/mnt/mtd/ipc/app/res/En
 [ -d /mnt/mtd/ipc/app/snd/english ] && WAV_FILE_DIR=/mnt/mtd/ipc/app/snd/english
@@ -17,14 +18,19 @@ WAV_FILE_DIR=/mnt/mtd/ipc/app/res/En
 mount --bind /tmp/di.wav $WAV_FILE_DIR/di.wav
 mount --bind /tmp/Internet_connected_Welcome_to_use_cloud_camera.wav $WAV_FILE_DIR/Internet_connected_Welcome_to_use_cloud_camera.wav
 mount --bind /tmp/WiFi_connect_success.wav $WAV_FILE_DIR/WiFi_connect_success.wav
+mount --bind /tmp/Please_use_mobile_phone_for_WiFi_configuration.wav $WAV_FILE_DIR/Please_use_mobile_phone_for_WiFi_configuration.wav
 
 # Add script for network management
 cp /mnt/mtd/ipc/app/script/dhcp.sh /tmp/dhcp.sh
 sed -i "s/udhcpc -a.*/\/mnt\/mmc\/network.sh \$1/g" /tmp/dhcp.sh
 mount --bind /tmp/dhcp.sh /mnt/mtd/ipc/app/script/dhcp.sh
 
+# Activate Wifi connection
+touch /tmp/DyVoiceRecog.bin
+touch /tmp/DyVoiceRecog.bin /mnt/mtd/ipc/cfg/DyVoiceRecog.bin
+
 # Remove colink binary
 touch /tmp/colink
 mount --bind /tmp/colink /mnt/mtd/ipc/app/colink
 
-(sleep 30 && /mnt/mmc/sonoff-hack/script/system.sh) &
+(sleep 20 && /mnt/mmc/sonoff-hack/script/system.sh) &
